ac7f153bbedf234f1f1d7c4830efcdc115bc0dec
jiaozhe <946505120@qq.com>
2021-06-23 12:13:53 +0800

    fix

--- packages/layer-photo/src/main.vue
+++ packages/layer-photo/src/main.vue
@@ -1,44 +1,51 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-23 11:13:01
- * @LastEditTime: 2021-06-23 11:16:44
- * @LastEditors: your name
+ * @LastEditTime: 2021-06-23 12:08:43
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fcwz-ui/packages/layer-photo/src/main.vue
 -->
 <template>
-  <svg :viewBox="`0 0 ${layer.width} ${layer.height}`"
-       :width="layer.width * scale"
-       :height="layer.height * scale"
-       preserveAspectRatio="xMinYMin meet"
-       :clip-path="clipPath"
-       class="el-photo-layer">
-    <g v-if="layer.data"
-       :transform="gTransform">
-      <image :xlink:href="layer.data.result"
-             :href="layer.data.result"
-             :width="layer.data.width"
-             :height="layer.data.height"
-             :transform="imageTransform"
-             preserveAspectRatio="xMinYMin"
-             image-rendering="optimizeQuality"></image>
+  <svg
+    :viewBox="`0 0 ${layer.width} ${layer.height}`"
+    :width="layer.width * scale"
+    :height="layer.height * scale"
+    preserveAspectRatio="xMinYMin meet"
+    :clip-path="clipPath"
+    class="el-photo-layer"
+  >
+    <g v-if="layer.data" :transform="gTransform">
+      <image
+        :xlink:href="layer.data.result"
+        :href="layer.data.result"
+        :width="layer.data.width"
+        :height="layer.data.height"
+        :transform="imageTransform"
+        preserveAspectRatio="xMinYMin"
+        image-rendering="optimizeQuality"
+      ></image>
     </g>
     <g v-else>
-      <rect :width="layer.width"
-            :height="layer.height"
-            :fill="view ? '#fff': '#D1D1D1'"></rect>
+      <rect
+        :width="layer.width"
+        :height="layer.height"
+        :fill="view ? '#fff' : '#D1D1D1'"
+      ></rect>
     </g>
-    <component v-if="layer.style"
-               :is="layer.style+'Mask'"
-               :layer="layer"></component>
+    <component
+      v-if="layer.style"
+      :is="layer.style + 'Mask'"
+      :layer="layer"
+    ></component>
   </svg>
 </template>
 
 <script>
-import circleMask from "./circle-mask";
-import ellipseMask from "./ellipse-mask";
+import circleMask from './circle-mask';
+import ellipseMask from './ellipse-mask';
 export default {
-  name: "ElLayerPhoto",
+  name: 'ElLayerPhoto',
   props: {
     layer: Object,
     scale: {
@@ -49,7 +56,7 @@ export default {
   },
   computed: {
     gTransform() {
-      let value = "";
+      let value = '';
       if (!this.layer.data) return value;
       let { rotate, x, y } = this.layer.data;
       x = -Math.abs(x);
@@ -59,16 +66,16 @@ export default {
         : `translate(${x} ${y})`;
     },
     imageTransform() {
-      let value = "";
+      let value = '';
       return value;
     },
     iconTransform() {
       // let { width, height } = this.layer;
-      return "";
+      return '';
     },
     clipPath() {
       const { style, id } = this.layer;
-      return style ? `url(#${id})` : "";
+      return style ? `url(#${id})` : '';
     }
   },
   components: {
@@ -77,4 +84,3 @@ export default {
   }
 };
 </script>
-
--- packages/page-edit/src/main.vue
+++ packages/page-edit/src/main.vue
@@ -1,29 +1,27 @@
 <!--
  * @Author: jiaozhe
  * @Date: 2021-06-23 10:55:28
- * @LastEditTime: 2021-06-23 11:09:11
+ * @LastEditTime: 2021-06-23 12:08:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fcwz-ui/packages/page-edit/src/main.vue
 -->
 <template>
-  <div :class="['fc-page-edit', {'is-view': view}]">
-    <div v-if="frame.isLine"
-         class="middle-line"></div>
-    <pg-bleed v-if="!bleed.borderWidth"
-              :bleed="bleed"></pg-bleed>
+  <div :class="['fc-page-edit', { 'is-view': view }]">
+    <div v-if="frame.isLine" class="middle-line"></div>
+    <pg-bleed v-if="!bleed.borderWidth" :bleed="bleed"></pg-bleed>
     <pg-border :border="frame.border"></pg-border>
-    <div class="el-page-wrap"
-         :style="size">
-      <div class="el-page-content"
-           :style="sizeWithBleed">
-        <pg-layout v-for="(item, index) in page.layers"
-                   :key="index"
-                   :pageId="page.id"
-                   :lIndex="index"
-                   :bleed="bleed"
-                   :scale="scale"
-                   :layout="item"></pg-layout>
+    <div class="el-page-wrap" :style="size">
+      <div class="el-page-content" :style="sizeWithBleed">
+        <pg-layout
+          v-for="(item, index) in page.layers"
+          :key="index"
+          :pageId="page.id"
+          :lIndex="index"
+          :bleed="bleed"
+          :scale="scale"
+          :layout="item"
+        ></pg-layout>
       </div>
     </div>
   </div>
@@ -31,7 +29,7 @@
 
 <script>
 export default {
-  name: "FcPageEdit",
+  name: 'FcPageEdit',
   props: {
     page: {
       type: Object,
@@ -47,10 +45,10 @@ export default {
       type: Object,
       default() {
         return {
-          output: "",
+          output: '',
           bleed: [4, 4, 4, 4],
           border: [40, 40, 40, 40],
-          type: ""
+          type: ''
         };
       }
     },
