42064173bf959911225a8f4b4774d1373f4b0ec4
undefined <undefined>
2021-08-04 18:01:19 +0800

    [build] 1.1.0-0

--- src/dacai.js
+++ src/dacai.js
@@ -21,7 +21,7 @@ const install = function(Vue, opts = {}) {
   });
 }
 export default {
-  version: '1.0.5',
+  version: '1.1.0-0',
   install,
   PageEdit,
   LayerPhoto,
--- src/index.js
+++ src/index.js
@@ -218,7 +218,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.5',
+  version: '1.1.0-0',
   locale: locale.use,
   i18n: locale.i18n,
   install,
