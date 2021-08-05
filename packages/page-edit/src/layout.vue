31db638ffc99db5bf8790d95fe76bf4d6a8eddcc
undefined <undefined>
2021-08-05 11:40:03 +0800

    修改layout.layer

--- packages/page-edit/src/layout.vue
+++ packages/page-edit/src/layout.vue
@@ -1,28 +1,33 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-23 16:15:03
- * @LastEditTime: 2021-07-01 21:16:22
+ * @LastEditTime: 2021-08-05 11:19:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fcwz-ui/packages/page-edit/src/layout.vue
 -->
 <template>
-  <div :data-index="layoutIndex"
-       :class="['pg-layout-item', { checked: checked, fill: layout.fill }]"
-       data-type="clear">
-    <div v-if="layout.id"
-         class="layout-item-content"
-         data-type="clear">
-      <pg-layer v-for="layer in layout.layer"
-                :key="layer.id"
-                :layoutIndex="layoutIndex"
-                :pageId="pageId"
-                :bleed="bleed"
-                :scale="scale"
-                :layer="layer"></pg-layer>
+  <div
+    :data-index="layoutIndex"
+    :class="['pg-layout-item', { checked: checked, fill: layout.fill }]"
+    data-type="clear"
+  >
+    <div
+      v-if="layout.layer.length"
+      class="layout-item-content"
+      data-type="clear"
+    >
+      <pg-layer
+        v-for="layer in layout.layer"
+        :key="layer.id"
+        :layoutIndex="layoutIndex"
+        :pageId="pageId"
+        :bleed="bleed"
+        :scale="scale"
+        :layer="layer"
+      ></pg-layer>
     </div>
-    <div v-else
-         class="layout-item-empty">
+    <div v-else class="layout-item-empty">
       点击或拖拽左侧图片，智能创建页面
     </div>
   </div>
