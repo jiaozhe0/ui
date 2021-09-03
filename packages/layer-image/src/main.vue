9f2dac440b01c044912b3fdc65c9e3de0e17ee96
undefined <undefined>
2021-09-03 14:08:20 +0800

    素材组件删除imageTransform

--- packages/layer-image/src/main.vue
+++ packages/layer-image/src/main.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-28 21:45:40
- * @LastEditTime: 2021-09-03 13:58:23
+ * @LastEditTime: 2021-09-03 14:04:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fcwz-ui/packages/layer-image/src/main.vue
@@ -19,7 +19,6 @@
       :href="layer.suolue_url"
       :width="layer.width"
       :height="layer.height"
-      :transform="imageTransform"
       preserveAspectRatio="xMinYMin"
       image-rendering="optimizeQuality"
     ></image>
